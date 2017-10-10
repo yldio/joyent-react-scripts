0a30994cbe3b40bff2bd6c65c4679a5b22d7f445
Sérgio Ramos <mail@sergioramos.me>
2017-10-10 15:45:45 +0100

    run react-scripts in a non-tty ctx

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 
 const main = require('apr-main');
+const execa = require('execa');
+const resolveFrom = require('resolve-from');
+const path = require('path');
+
 const postinstall = require('./src/postinstall');
+const bin = resolveFrom(__dirname, 'react-scripts/bin/react-scripts');
+const args = process.argv.slice(2);
 
 main(() =>
   (async () => {
     await postinstall();
 
-    require('react-scripts/bin/react-scripts');
+    const { stdout } = execa(bin, args, {
+      stdio: ['ignore', 'pipe', process.stderr],
+      env: process.env
+    });
+
+    stdout.pipe(process.stdout);
   })()
 );
