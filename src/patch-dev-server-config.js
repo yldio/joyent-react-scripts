b367278da3f593f104325273cea7c345e0dd6aee
Sérgio Ramos <mail@sergioramos.me>
2017-12-01 01:57:59 +0000

    fix font proxying

--- src/patch-dev-server-config.js
+++ src/patch-dev-server-config.js
@@ -1,4 +1,4 @@
-const got = require('got');
+const request = require('request');
 const url = require('url');
 
 module.exports = originalConfigFn => (proxy, allowedHost) => {
@@ -6,47 +6,44 @@ module.exports = originalConfigFn => (proxy, allowedHost) => {
 
   return Object.assign({}, originalConf, {
     before: (app, self) => {
-      app.get('/font/*', ({ params }, res) => {
-        got
-          .stream(
-            url.format({
-              protocol: 'https:',
-              slashes: true,
-              host: 'fonts.gstatic.com',
-              pathname: params['0']
-            })
-          )
-          .pipe(res);
+      app.get('/font/*', ({ params, headers, ...req }, res, next) => {
+        request(url.format({
+          protocol: 'https:',
+          slashes: true,
+          host: 'fonts.gstatic.com',
+          pathname: params['0']
+        })).pipe(res);
       });
 
       app.get('/fonts/css', ({ query, connection }, res, next) => {
         const { family } = query;
         const { address, port } = connection.server.address();
 
-        got(
-          url.format({
-            protocol: 'https:',
-            slashes: true,
-            host: 'fonts.googleapis.com',
-            pathname: '/css',
-            query: {
-              family
-            }
-          })
-        )
-          .then(({ body, headers }) => {
-            res.setHeader('content-type', headers['content-type']);
-            res.setHeader('expires', headers['expires']);
-            res.setHeader('date', headers['date']);
-            res.setHeader('cache-control', headers['cache-control']);
+        request(url.format({
+          protocol: 'https:',
+          slashes: true,
+          host: 'fonts.googleapis.com',
+          pathname: '/css',
+          query: {
+            family
+          }
+        }), (err, { body, headers }) => {
+          if (err) {
+            return next(err);
+          }
 
-            const newBody = body.replace(
-              /https\:\/\/fonts\.gstatic\.com/g,
-              `http://${address}:${port}/font`
-            );
-            res.end(newBody);
-          })
-          .catch(err => next(err));
+          res.setHeader('content-type', headers['content-type']);
+          res.setHeader('expires', headers['expires']);
+          res.setHeader('date', headers['date']);
+          res.setHeader('cache-control', headers['cache-control']);
+
+          const newBody = body.replace(
+            /https\:\/\/fonts\.gstatic\.com/g,
+            `http://${address}:${port}/font`
+          );
+
+          res.end(newBody);
+        });
       });
 
       if (originalConf.before) {
