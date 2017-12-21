e40b6e0fac56dc2fcecd1a831447def3ea18bcfa
Sérgio Ramos <mail@sergioramos.me>
2017-12-21 19:39:36 +0000

    inherit stdio when test

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const postinstall = require('./src/postinstall');
 const bin = resolveFrom(__dirname, 'react-scripts/bin/react-scripts');
 const args = process.argv.slice(2);
 
+const scriptIndex = args.findIndex(
+  x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
+);
+
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+
 main(() =>
   (async () => {
     await postinstall();
+
+    if (script === 'test') {
+      return execa(bin, args, {
+        stdio: 'inherit',
+        env: process.env
+      });
+    }
 
     const { stdout } = execa(bin, args, {
       stdio: ['ignore', 'pipe', process.stderr],
